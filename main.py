abe3decd28c8965991d0479c4c07acd9135be629
indralingga <indralinggasaputraa@gmail.com>
2020-10-13 04:43:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+x = int(input("Masukkan bilangan pertama: "))
+y = int(input("Masukkan bilangan kedua: "))
+z = int(input("Masukkan bilangan ketiga: "))
+
+average = (x + y + z) / 3
+print('Rata-rata bilangan adalah', average)
+
+# SOAL 2 - Menulis kelipatan bilangan
+# Tuliskan program untuk Soal 2 di bawah ini
+angka = 5
+
+while angka <= 25:
+    print(angka, sep="---", end="---")
+    angka = angka + 5
