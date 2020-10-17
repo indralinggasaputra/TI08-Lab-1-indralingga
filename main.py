92e6bbde233d9a4e77128290b67a48c8d211b387
indralingga <indralinggasaputraa@gmail.com>
2020-10-17 09:23:41 +0000

    average

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ y = int(input("Masukkan bilangan kedua: "))
 z = int(input("Masukkan bilangan ketiga: "))
 
 average = (x + y + z) / 3
-print('Rata-rata bilangan adalah', average)
+print('Rata-rata bilangan adalah {0}, {1}, dan {2} adalah {3}'.format(
+    x, y, z, average))
 
 # SOAL 2 - Menulis kelipatan bilangan
 # Tuliskan program untuk Soal 2 di bawah ini
 angka = 5
 
 while angka <= 25:
+    print("Kelipatan angka 5 adalah")
     print(angka, sep="---", end="---")
     angka = angka + 5
